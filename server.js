5ff1c9bf3df295f8c301cb63bc8879e03805e19c
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-12-05 21:42:41 -0600

    server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/FriendFinder/app/public/home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
   });
   
 
